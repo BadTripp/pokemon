9248f60df3dc312cb2d34b291b19bfde6ab5b333
BadTripp <renato.turetta00@gmail.com>
2022-07-09 15:21:07 +0200

    fix moveslist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PokemonListComponent } from '../component/feature/pokemon-list/pokemon-list.component';
 import { PlayComponent } from '../component/feature/play/play.component';
-import { MovesListComponent } from './moves-list/moves-list.component';
+import { MovesListComponent } from 'src/component/feature/moves-list/moves-list.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
